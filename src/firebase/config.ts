c78fc9c7869ce5e25932dcf4754324b0bcf40abb
Nathaniel Senje <topsailcashew@gmail.com>
2025-11-14 08:47:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6491013315-e8442",
+  "appId": "1:658261854815:web:65af76b9692c334652a9ea",
+  "apiKey": "AIzaSyAXhq5VOXUJPW7R_vn_deFGG2COe8EAgVk",
+  "authDomain": "studio-6491013315-e8442.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "658261854815"
+};
